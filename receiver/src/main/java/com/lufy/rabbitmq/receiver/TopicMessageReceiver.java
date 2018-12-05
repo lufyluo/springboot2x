ea4424d52da2f266a1fcef01690204fa7f275b43
lufyluo <lufyl@163.com>
2018-12-05 16:25:19 +0800

    1、提交rabbitMQ topic

--- receiver/src/main/java/com/lufy/rabbitmq/receiver/TopicMessageReceiver.java
+++ receiver/src/main/java/com/lufy/rabbitmq/receiver/TopicMessageReceiver.java
@@ -5,12 +5,16 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@RabbitListener(queues = "topic.message")
-public class topicMessageReceiver {
 
-    @RabbitHandler
+public class TopicMessageReceiver {
+
+    @RabbitListener(queues = "topic.message")
     public void process(String msg) {
         System.out.println("topicMessageReceiver  : " +msg);
+    }
+    @RabbitListener(queues = "topic.messages")
+    public void process1(String msg) {
+        System.out.println("topicMessageReceiver s  : " +msg);
     }
 
 }
